445a1654fa164af948ddb96f7b52e735bc88b012
GitLucaI <circusbabyyt589@gmail.com>
2024-09-27 22:44:19 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,8 +6,9 @@ local Progressbar = loader.Progress.Bar
 local lmain = loader.Main
 local lsub = loader.Sub
 local lbutton = loader.Btn
-
+local status = "loading"
 function fail(err, desc)
+	status = "failed"
 	lbutton.BackgroundColor3 = Color3.fromRGB(155, 0, 0)
 	lbutton.TextTransparency = 0
 	lbutton.Interactable = true
@@ -17,11 +18,31 @@ function fail(err, desc)
 end
 
 function retry()
-	loadstring()
+	loadstring(game:httpget('https://raw.githubusercontent.com/GitLucaI/Synapse-Hub/refs/heads/main/main.lua'))()
+	task.wait()
+	object:Destroy()
 end
+
+function boot()
+end
+
 
 lbutton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
 lbutton.TextTransparency = 0.5
 lbutton.Interactable = false
+local t = math.random(1,2)
+if t == 2 then
+	fail("This is a error", "We are sorry")
+end
 
-fail("This is a error", "We are sorry")
+
+lbutton.MouseButton1Click:Connect(function()
+	if status == "loading" then
+		return
+	end
+	if status == "failed" then
+		retry()
+	elseif status == "loaded" then
+		boot()
+	end
+end)
